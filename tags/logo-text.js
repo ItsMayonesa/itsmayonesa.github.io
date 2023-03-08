fbdf742e0f6447a4b57b70813527025256449473
ItsMayonesa <78627124+ItsMayonesa@users.noreply.github.com>
2023-03-08 18:26:13 -0300

    Delete tags directory
    Rewrite site

--- tags/logo-text.js
+++
@@ -1,8 +0,0 @@
-class LogoText extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML =  `<p>Hello!!!!!</p>`
-        this.style.color = "red"
-    }
-}
-
-customElements.define("logo-element", LogoText)
